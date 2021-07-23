d19537e15387e5bb64dc314a51f1378eb63ba44d
Sam Lo <saint123600@hotmail.com>
2021-07-23 22:28:25 +0800

    calc average reviews

---
+++ read.py
@@ -0,0 +1,13 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 10000 == 0:
+			print(len(data))
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均是', sum_len/len(data))	
+        
